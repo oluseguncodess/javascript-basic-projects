867aa7781e8d8010c4248c20af45c272af47cee6
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-03 23:34:51 +0100

    update

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,16 @@
+//Start by accessing the buttons and counter value
+let count = 0; 
+
+//Access all the buttons
+const btns = document.querySelectorAll(".btn");
+
+
+// Access the counter value
+const counter = document.getElementById("value");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        
+    })
+});
+
